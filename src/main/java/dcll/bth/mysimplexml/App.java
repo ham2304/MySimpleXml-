0648cf9a4bbd9bcb6c0a4c031c879f640abd8b28
Bah Thierno <ham2304@gmail.com>
2012-02-29 14:23:07 +0100

    Premier Commit

---
+++ src/main/java/dcll/bth/mysimplexml/App.java
@@ -0,0 +1,13 @@
+package dcll.bth.mysimplexml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
